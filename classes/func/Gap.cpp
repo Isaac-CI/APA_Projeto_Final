a2e7d8708301213812a85f6999ff9d539c88f791
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2022-06-15 03:44:07 -0300

    Defining gap class

---
+++ classes/func/Gap.cpp
@@ -0,0 +1,24 @@
+#include "../interfaces/Gap.h"
+#include "../interfaces/Greedy.h"
+#include <vector>
+
+std::vector <int> optimalValues = {1200, 240, 243, 413, 377, 928, 958};
+
+Gap::Gap(Solution heuristicValue, int instanceID){
+  try {
+    this->heuristicValue = heuristicValue;
+    this->optimalValue = optimalValues[instanceID-1];
+  }
+  catch(const std::exception& e) {
+    std::cerr << e.what() << '\n';
+  }
+}
+
+void Gap::printGap(){
+  double gap = ((this->heuristicValue.solutionCost-this->optimalValue)
+    /this->optimalValue)*100;
+  
+  std::cout  << "Solução encontrada: " << this->heuristicValue.solutionCost << std::endl;
+  std::cout  << "Solução ótima: " << this->optimalValue << std::endl;
+  std::cout  << gap << "%" << std::endl;
+}
