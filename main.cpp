5dd7c20174604a2b7c9152ff129f033b871717e0
Isaac-CI <isaacmarinho@eng.ci.ufpb.br>
2022-06-16 23:14:06 -0300

    Enhanced Output

--- main.cpp
+++ main.cpp
@@ -11,26 +11,22 @@
 #define INSTANCE 7
 
 int main(void){
-<<<<<<< HEAD
     srand(time(NULL));
-    JobXServer instance = JobXServer("./instances/input8.txt");  
-=======
-    JobXServer instance = JobXServer("./instances/input7.txt");  
->>>>>>> 1969e6d73be47a2ea4e854cad8931b9db4018330
+    JobXServer instance = JobXServer("./instances/input2.txt");  
 
     auto greedStart = std::chrono::high_resolution_clock::now();
     Greedy guloso = Greedy(instance);
     auto greedFinish = std::chrono::high_resolution_clock::now();
     auto greedDuration = std::chrono::duration_cast<std::chrono::microseconds>(greedFinish - greedStart);
     std::cout << "Solução Gulosa encontrada em "<< greedDuration.count() << " us: " << std::endl;
-    std::cout << "Custo do médio do Greedy: " << guloso.solution.solutionCost << std::endl;
+    std::cout << "Custo da solução Gulosa: " << guloso.solution.solutionCost << std::endl;
     std::cout << "\n";
 
     //-------------------------------- VND ---------------------------------------
     Vnd VND = Vnd(guloso.solution, guloso.data);
 
         std::cout << "Solução alcançada pelo VND em " << VND.executionTime.count() << " us: " << std::endl;
-        std::cout << "Custo do médio do  VND: " << VND.solution.solutionCost << std::endl;
+        std::cout << "Melhor custo encontrado pelo VND: " << VND.solution.solutionCost << std::endl;
         std::cout << "\n";
 
 
@@ -41,8 +37,6 @@ int main(void){
         while(currentIter - lastIterWithBetterSolution < THRESHOLD){    
             Grasp arrochado = Grasp(instance);
             Vnd graspVND = Vnd(arrochado.solution, instance);
-            std::cout << "Grasp " << currentIter << " cost: " << graspVND.solution.solutionCost << std::endl;
-            std::cout << "\n";
             if(graspVND.solution.solutionCost < best.solutionCost){
                 lastIterWithBetterSolution = currentIter;
                 best = graspVND.solution;
@@ -51,8 +45,8 @@ int main(void){
         }
         auto stopGrasp = std::chrono::high_resolution_clock::now();
         auto GraspDuration = std::chrono::duration_cast<std::chrono::microseconds>(stopGrasp - startGrasp);
-        std::cout << "Solução alcançada pelo grasp VND em " << GraspDuration.count() << " us: " << std::endl;
-        std::cout << "Custo do médio do Grasp VND: " << best.solutionCost << std::endl;
+        std::cout << "Solução alcançada pelo Grasp + VND em " << GraspDuration.count() << " us: " << std::endl;
+        std::cout << "Melhor custo encotrado pelo Grasp + VND: " << best.solutionCost << std::endl;
 
         Gap gap = Gap(VND.solution, INSTANCE);
         gap.printGap();
